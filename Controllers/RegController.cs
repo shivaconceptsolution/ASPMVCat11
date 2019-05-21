5e75a8aa3c3d01f35f18f63e492a045aa34d50c9
shivaconceptsolution <50239824+shivaconceptsolution@users.noreply.github.com>
2019-05-21 10:57:32 +0530

    Add files via upload

---
+++ Controllers/RegController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DatabaseConnectivityExample.Models;
+namespace DatabaseConnectivityExample.Controllers
+{
+    public class RegController : Controller
+    {
+        Database1Entities db = new Database1Entities();
+        // GET: Reg
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Reg r)
+        {
+            var s = (from c in db.Regs where c.Email.Equals(r.Email) select c).FirstOrDefault();
+            if (s == null)
+            {
+                db.Regs.Add(r);
+                db.SaveChanges();
+                ViewBag.data = "Registration SuccessFully";
+            }
+            else
+            {
+                ViewBag.data = "EmailID Already Exist";
+            }
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Reg r)
+        {
+            var s = (from c in db.Regs where c.Email.Equals(r.Email) && c.Pass.Equals(r.Pass) select c).FirstOrDefault();
+            if(s!=null)
+            {
+                TempData["uid"] = r.Email;
+                TempData.Keep();
+                return RedirectToAction("Index", "UserDash");
+            }
+            else
+            {
+                ViewBag.data = "Invalid emailid and password";
+            }
+            return View();
+        }
+
+    }
+}
---
+++ Controllers/UserDashController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DatabaseConnectivityExample.Models;
+namespace DatabaseConnectivityExample.Controllers
+{
+    public class UserDashController : Controller
+    {
+        Database1Entities db = new Database1Entities();
+        
+        // GET: UserDash
+        public ActionResult Index()
+        {
+            if (TempData["uid"] == null)
+                return RedirectToAction("Login", "Reg");
+            else
+            {
+                TempData.Keep();
+                String uid = TempData["uid"].ToString();
+
+                var s = (from c in db.Regs where c.Email.Equals(uid) select c).FirstOrDefault();
+                return View(s);
+            }
+        }
+        public ActionResult Logout()
+        {
+            TempData.Remove("uid");
+            return RedirectToAction("Login", "Reg");
+
+        }
+    }
+}
