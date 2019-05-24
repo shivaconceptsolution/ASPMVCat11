37544137dd53f3fb35dc9f853599d1a145776e11
shivaconceptsolution <50239824+shivaconceptsolution@users.noreply.github.com>
2019-05-24 12:23:10 +0530

    Add files via upload

---
+++ Models/AppointMent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DatabaseConnectivityExample.Models
+{
+    public class AppointMent
+    {
+        public int PateientId { get; set; }
+        public string PatirntName { get; set; }
+        public string ContactNumber { get; set; }
+        public int DoctorId { get; set; }
+        public string DoctorName { get; set; }
+        public string MedicineName { get; set; }
+        public DateTime VisitingDate { get; set; }
+        public int DoctorFee { get; set; }
+
+
+    }
+}
