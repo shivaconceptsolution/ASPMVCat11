0f7bb5f6ba371dd36ada4099013bc4eaa670ef1d
shivaconceptsolution <50239824+shivaconceptsolution@users.noreply.github.com>
2019-06-10 16:15:07 +0530

    Add files via upload

---
+++ Controllers/AjaxExampleController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication25.Models;
+namespace WebApplication25.Controllers
+{
+    public class AjaxExampleController : Controller
+    {
+
+        SchoolDB db = new SchoolDB();
+        // GET: AjaxExample
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Search(string id)
+        {
+            var s = from c in db.Students where c.Sname.StartsWith(id) select c;
+            return PartialView(s.ToList());
+        }
+
+    }
+}
