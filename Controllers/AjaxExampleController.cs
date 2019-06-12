50783ba253660c81738c7633ca157d4edb927113
shivaconceptsolution <50239824+shivaconceptsolution@users.noreply.github.com>
2019-06-12 15:57:13 +0530

    Add files via upload

--- Controllers/AjaxExampleController.cs
+++ Controllers/AjaxExampleController.cs
@@ -13,6 +13,8 @@ namespace WebApplication25.Controllers
         // GET: AjaxExample
         public ActionResult Index()
         {
+           
+            ViewBag.data = new SelectList( db.Students,"Sname","Sname" );      
             return View();
         }
         public ActionResult Search(string id)
@@ -20,6 +22,21 @@ namespace WebApplication25.Controllers
             var s = from c in db.Students where c.Sname.StartsWith(id) select c;
             return PartialView(s.ToList());
         }
-
+        public ActionResult SubmitData()
+        {
+            return View();
+        }
+        public ActionResult InsertData()
+        {
+            Student s = new Student();
+            s.Rno = int.Parse(Request["rno"]);
+            s.Sname = Request["name"];
+            s.Branch = Request["branch"];
+            s.Fees = int.Parse(Request["fees"]);
+            db.Students.Add(s);
+            db.SaveChanges();
+            ViewBag.data = "Data Inserted Successfully";
+            return PartialView();
+        }
     }
 }
